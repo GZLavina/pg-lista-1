412c51fb9cddff9eef40873b6791c03be62dfd8f
Gustavo Lavina <gz.lavina@gmail.com>
2024-03-28 18:18:19 -0300

    final commit before lista 1

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
     glfwSetErrorCallback(error_callback);
 
     // glfw window creation
-    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", nullptr, nullptr);
     if (window == nullptr) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -44,7 +44,7 @@ int main() {
     Shader shader_("../shaders/HelloTriangle.vert", "../shaders/HelloTriangle.frag");
 
     float vertices[] = {
-            // positions         // colors
+            // positions        // colors
              0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
             -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
              0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top
@@ -60,7 +60,7 @@ int main() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)nullptr);
     glEnableVertexAttribArray(0);
     // color attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
@@ -73,7 +73,7 @@ int main() {
         // input
         processInput(window);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBindVertexArray(VAO);
